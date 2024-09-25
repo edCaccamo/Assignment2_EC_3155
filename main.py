29ac9dba0dc8e7c04f81f544658248fb06a2c86b
edCaccamo <ecaccamo144@gmail.com>
2024-09-25 19:38:23 -0400

    Finished Main, Everything done

--- main.py
+++ main.py
@@ -6,14 +6,31 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = SandwichMaker(resources)
+cashier_instance = Cashier()
 
 
 
 
 def main():
-    ###  write the rest of the codes ###
+    is_on = True
+    while is_on:
+        choice = input("What would you like? (small/ medium/ large/ off/ report): ").lower()
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            print(f"Bread: {resources['bread']} slices")
+            print(f"Ham: {resources['ham']} slices")
+            print(f"Cheese: {resources['cheese']} pounds")
+        elif choice in ["small", "medium", "large"]:
+            sandwich = recipes[choice]
+            if sandwich_maker_instance.check_resources(sandwich["ingredients"]):
+                payment = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(payment, sandwich["cost"]):
+                    sandwich_maker_instance.make_sandwich(choice, sandwich["ingredients"])
+                    print(f"{choice} sandwich is ready. Bon appetit!")
+        else:
+            print("Invalid choice.")
 
 if __name__=="__main__":
     main()
